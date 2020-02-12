9aadd94c2ac9a0c55981b148f434e12d64bbf13e
sandraelekes <60325591+sandraelekes@users.noreply.github.com>
2020-02-12 22:54:24 +0000

    Program checks is a number odd or even
    Program checkes if positive integer number is odd or even and performs actions accordingly.

--- 4-collatz.py
+++ 4-collatz.py
@@ -1,4 +1,4 @@
-# Program that asks the user to input any positive integer and
+# Program asks the user to input any positive integer and
 # outputs the successive values of the following calculation.
 # At each step calculate the next value by taking the current value and,
 # if it is even, divide it by two,
