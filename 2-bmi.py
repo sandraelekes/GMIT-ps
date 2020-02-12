cee10e31c8d165e6565405de7f32b845dd8b60b3
sandraelekes <60325591+sandraelekes@users.noreply.github.com>
2020-02-12 22:39:35 +0000

    Update 2-bmi.py
    Edited code and comments.

--- 2-bmi.py
+++ 2-bmi.py
@@ -4,15 +4,14 @@
 weight = float (input("Enter your weight in kg: "))
 height = float (input("Enter your height in cm: "))
 
-# BMI calculation is weight in centimeters devided by height in meters squared
-# hm = height in meters
-
+# conversion of hight from cm to m using variable hm
 hm = height/100
 
+# BMI calculation is weight in kilograms devided by height in meters squared
 BMI = weight / (hm**2)
 
-# rounding of BMI to 2 points
+print ("Your BMI is", round (BMI,2))
 
-BMIr = (round (BMI,2))
-
-print ("Your BMI is", BMIr)
+# credit for rounding BMI to 2 poins idea goes to to another student Jonathan Harper
+# more information on rounding found on:
+# https://www.geeksforgeeks.org/round-function-python/
